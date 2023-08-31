84935abdaa2f22459c4d48d30a1de28ccf541e2b
tomit4 <mosssap@gmail.com>
2023-08-30 23:50:08 -0700

    :wrench: Put helmet on

--- backend/lib/plugins/index.js
+++ backend/lib/plugins/index.js
@@ -1,6 +1,7 @@
 'use strict'
 // TODO: Provide proper jsdocs style comments here
 module.exports = async fastify => {
+    await fastify.register(require('@fastify/helmet'))
     await fastify.register(require('@fastify/swagger'))
     await fastify.register(require('@fastify/swagger-ui'))
     await fastify.register(require('./knex'))
