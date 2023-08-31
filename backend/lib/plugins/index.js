2cb73096aca987ef26b3fc2a3a2e4fa15533fc9e
tomit4 <mosssap@gmail.com>
2023-08-31 00:28:49 -0700

    :wrench: Added rate limit plugin

--- backend/lib/plugins/index.js
+++ backend/lib/plugins/index.js
@@ -2,6 +2,10 @@
 // TODO: Provide proper jsdocs style comments here
 module.exports = async fastify => {
     await fastify.register(require('@fastify/helmet'))
+    await fastify.register(require('@fastify/rate-limit'), {
+        max: 100,
+        timeWindow: '1 minute',
+    })
     await fastify.register(require('@fastify/swagger'))
     await fastify.register(require('@fastify/swagger-ui'))
     await fastify.register(require('./knex'))
