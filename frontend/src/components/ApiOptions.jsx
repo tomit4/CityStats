1aceb2b4f9ac5ea9eade91eecdc112f875cda327
tomit4 <mosssap@gmail.com>
2023-11-27 01:40:31 -0800

    :bug: Fixed bug with hash-router-link

--- frontend/src/components/ApiOptions.jsx
+++ frontend/src/components/ApiOptions.jsx
@@ -1,45 +1,45 @@
 import PropTypes from 'prop-types'
-import { NavHashLink } from 'react-router-hash-link'
+import { HashLink } from 'react-router-hash-link'
 
 const ApiOptions = props => {
     if (props.showStatesLinks) {
         return (
             <>
                 <div className="nav-menu-title">States API Options</div>
-                <NavHashLink
+                <HashLink
                     onClick={props.toggleFromAnchor}
                     className="page-nav nav-link"
                     to="/states#test"
                 >
                     States Element One
-                </NavHashLink>
-                <NavHashLink
+                </HashLink>
+                <HashLink
                     onClick={props.toggleFromAnchor}
                     className="page-nav nav-link"
                     to="/states#test"
                 >
                     States Element Two
-                </NavHashLink>
+                </HashLink>
             </>
         )
     } else {
         return (
             <>
                 <div className="nav-menu-title">Cities API Options</div>
-                <NavHashLink
+                <HashLink
                     onClick={props.toggleFromAnchor}
                     className="page-nav nav-link"
                     to="/cities#test"
                 >
                     Cities Element One
-                </NavHashLink>
-                <NavHashLink
+                </HashLink>
+                <HashLink
                     onClick={props.toggleFromAnchor}
                     className="page-nav nav-link"
                     to="/cities#test"
                 >
                     Cities Element Two
-                </NavHashLink>
+                </HashLink>
             </>
         )
     }
