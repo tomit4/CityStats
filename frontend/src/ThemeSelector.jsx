c421d539cdf0fd41f9f7e79972303bce6246a74c
tomit4 <mosssap@gmail.com>
2023-12-07 00:24:11 -0800

    :pencil2: Subtle refactor removing unnecessary code

--- frontend/src/ThemeSelector.jsx
+++ frontend/src/ThemeSelector.jsx
@@ -2,14 +2,9 @@ import React, { useState, useContext, createContext } from 'react'
 import PropTypes from 'prop-types'
 const LightTheme = React.lazy(() => import('./themes/Lighttheme.jsx'))
 const DarkTheme = React.lazy(() => import('./themes/Darktheme.jsx'))
-
-const ThemeContext = createContext()
 const ThemeUpdateContext = createContext()
 
 /* TODO: Address fast refresh warnings from eslint */
-export function useTheme() {
-    return useContext(ThemeContext)
-}
 export function useThemeUpdate() {
     return useContext(ThemeUpdateContext)
 }
@@ -18,14 +13,9 @@ const ThemeSelector = ({ children }) => {
     const [prefersDark, setPrefersDark] = useState(
         window.matchMedia('(prefers-color-scheme: dark)').matches,
     )
-    const [prefersLight, setPrefersLight] = useState(
-        window.matchMedia('(prefers-color-scheme: light)').matches,
-    )
 
-    /* NOTE: This problem has been thought of extensively by
-     * others far more skilled than you (see ../README.md)
-     * Hacky workaround to get prismjs stylesheets to
-     * toggle on and off depending on dark/light mode */
+    /* NOTE: Hacky workaround to get prismjs stylesheets to
+     * toggle on and off depending on dark/light mode (see ./README.md) */
     const toggleTheme = () => {
         if (document.styleSheets.length === 3) {
             document.styleSheets[2].disabled = true
@@ -34,21 +24,18 @@ const ThemeSelector = ({ children }) => {
             document.styleSheets[3].disabled = document.styleSheets[2].disabled
             document.styleSheets[2].disabled = !document.styleSheets[2].disabled
         }
-        setPrefersLight(prevLightTheme => prevLightTheme)
         setPrefersDark(prevDarkTheme => !prevDarkTheme)
     }
 
     return (
         <>
-            <ThemeContext.Provider value={prefersDark}>
-                <ThemeUpdateContext.Provider value={toggleTheme}>
-                    <React.Suspense fallback={<div />}>
-                        {prefersLight && <LightTheme />}
-                        {prefersDark && <DarkTheme />}
-                    </React.Suspense>
-                    {children}
-                </ThemeUpdateContext.Provider>
-            </ThemeContext.Provider>
+            <ThemeUpdateContext.Provider value={toggleTheme}>
+                <React.Suspense fallback={<div />}>
+                    {!prefersDark && <LightTheme />}
+                    {prefersDark && <DarkTheme />}
+                </React.Suspense>
+                {children}
+            </ThemeUpdateContext.Provider>
         </>
     )
 }
