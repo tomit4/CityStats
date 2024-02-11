e1b5457e305e9d52886bf72f2cae7460519230b9
tomit4 <mosssap@gmail.com>
2024-02-11 04:37:48 -0800

    :bento: Added dark bg for splash page

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -2,15 +2,22 @@ import { Link } from 'react-router-dom'
 import '../css/Splash.css'
 
 export default function Splash() {
+    const theme = localStorage.getItem('data-citystats-theme')
     return (
         <>
-            <section className="home-hero">
-                <div className="container">
-                    <h1 className="title">CityStats API</h1>
-                    <span className="subtitle">
+            <section data-citystats-theme={theme} className="home-hero">
+                <div data-citystats-theme={theme} className="container">
+                    <h1 data-citystats-theme={theme} className="title">
+                        CityStats API
+                    </h1>
+                    <span data-citystats-theme={theme} className="subtitle">
                         Get Statistics On US States And Cities
                     </span>
-                    <Link className="link" to="/states">
+                    <Link
+                        data-citystats-theme={theme}
+                        className="link"
+                        to="/states"
+                    >
                         Click Here To Get Started
                     </Link>
                 </div>
