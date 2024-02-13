5a748d89ddafd345f4854100f6852d21cb16226c
tomit4 <mosssap@gmail.com>
2024-02-13 01:28:17 -0800

    :lipstick: Styled footer more

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,16 @@ export default function Footer() {
                     <li>
                         <a
                             data-citystats-theme={theme}
-                            className="footer-links-list-item"
+                            className="footer-links-list-item footer-icons"
+                            id="email-icon"
+                            aria-label="email"
+                            role="navigation"
+                            href="mailto: brianhayes.dev@protonmail.com"
+                        />
+                        <br />
+                        <a
+                            data-citystats-theme={theme}
+                            className="footer-links-list-item footer-links-text"
                             aria-label="email"
                             role="navigation"
                             href="mailto: brianhayes.dev@protonmail.com"
@@ -20,7 +29,18 @@ export default function Footer() {
                     <li>
                         <a
                             data-citystats-theme={theme}
-                            className="footer-links-list-item"
+                            className="footer-links-list-item footer-icons"
+                            id="git-icon"
+                            aria-label="A Link To Creator's Github Profile"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            role="link"
+                            href="https://github.com/tomit4"
+                        />
+                        <br />
+                        <a
+                            data-citystats-theme={theme}
+                            className="footer-links-list-item footer-links-text"
                             aria-label="A Link To Creator's Github Profile"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -33,7 +53,18 @@ export default function Footer() {
                     <li>
                         <a
                             data-citystats-theme={theme}
-                            className="footer-links-list-item"
+                            className="footer-links-list-item footer-icons"
+                            id="linkedin-icon"
+                            aria-label="A Link To Creator's LinkedIn Profile"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            role="link"
+                            href="https://linkedin.com/in/brian-hayes-33496067"
+                        />
+                        <br />
+                        <a
+                            data-citystats-theme={theme}
+                            className="footer-links-list-item footer-links-text"
                             aria-label="A Link To Creator's LinkedIn Profile"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -46,7 +77,18 @@ export default function Footer() {
                     <li>
                         <a
                             data-citystats-theme={theme}
-                            className="footer-links-list-item"
+                            className="footer-links-list-item footer-icons"
+                            id="blog-icon"
+                            aria-label="A Link To The Creator's Website/Blog"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            role="link"
+                            href="https://brianhayes.dev"
+                        />
+                        <br />
+                        <a
+                            data-citystats-theme={theme}
+                            className="footer-links-list-item footer-links-text"
                             aria-label="A Link To The Creator's Website/Blog"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -59,7 +101,18 @@ export default function Footer() {
                     <li>
                         <a
                             data-citystats-theme={theme}
-                            className="footer-links-list-item"
+                            className="footer-links-list-item footer-icons"
+                            id="mastodon-icon"
+                            aria-label="A Link To The Creator's Mastodon Profile"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            role="link"
+                            href="https://mas.to/@brianhayesdev"
+                        />
+                        <br />
+                        <a
+                            data-citystats-theme={theme}
+                            className="footer-links-list-item footer-links-text"
                             aria-label="A Link To The Creator's Mastodon Profile"
                             target="_blank"
                             rel="noopener noreferrer"
