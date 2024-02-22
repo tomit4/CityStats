83b876b6b488ba4ae9783d7ad840ad22f4330ed4
tomit4 <mosssap@gmail.com>
2024-02-22 07:21:31 -0800

    :lipstick: Styled not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,18 @@
 /* v8 ignore next 8 */
+// TODO: Change above number once done editing file:
+import '../css/Splash.css'
+
 export default function NotFound() {
     return (
         <>
-            <h3 className="page-title">404 Resource Not Found</h3>
-            <div className="section-divider" />
+            <div className="not-found-container">
+                <h3 className="page-title not-found-title">404</h3>
+                <br />
+                <h4 className="page-subtitle not-found-sub">
+                    Resource Not Found
+                </h4>
+                <div className="section-divider" />
+            </div>
         </>
     )
 }
