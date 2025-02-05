1bcaacab57afbc1053ba106ce10260dc510d1f46
tomit4 <mosssap@gmail.com>
2025-02-05 12:04:27 -0800

    :memo: Made note of better way to error handle context

--- frontend/src/contexts/useThemeUpdate.jsx
+++ frontend/src/contexts/useThemeUpdate.jsx
@@ -1,5 +1,9 @@
 import { useContext } from 'react'
 import { ThemeUpdateContext } from './ThemeUpdateContext'
 export function useThemeUpdate() {
+    // NOTE: Better way to ensure returnVal of ThemeUpdateContext is not undefined
+    // const themeContext = useContext(ThemeUpdateContext);
+    // if (themeContext === undefined) throw new Error("ThemeSelector must wrap main App.jsx")
+    // return themeContext
     return useContext(ThemeUpdateContext)
 }
